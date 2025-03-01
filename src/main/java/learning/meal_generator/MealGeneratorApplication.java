a7365a107f9a287a88aef247de51475035ccaa92
Billy Roberts <billyjamesroberts@hotmail.com>
2025-03-01 17:19:28 +0000

    Initial Commit

---
+++ src/main/java/learning/meal_generator/MealGeneratorApplication.java
@@ -0,0 +1,13 @@
+package learning.meal_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/learning/meal_generator/MealGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package learning.meal_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
